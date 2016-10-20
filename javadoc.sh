72bf557d5d99d14e702e15798531f18c31d7bfe6
Moxie Marlinspike <moxie@thoughtcrime.org>
2016-10-19 17:14:27 -0700

    Updated javadoc

--- javadoc.sh
+++ javadoc.sh
@@ -1,4 +1,4 @@
 rm -rf ./tmp/
-git clone https://github.com/WhisperSystems/libaxolotl-java.git tmp
-javadoc -doclet com.google.doclava.Doclava -docletpath doclava-1.0.6.jar -d . -sourcepath tmp/java/src/main/java/ -subpackages org -hdf project.name "libaxolotl-java"
+git clone https://github.com/WhisperSystems/libsignal-protocol-java.git tmp
+javadoc -doclet com.google.doclava.Doclava -docletpath doclava-1.0.6.jar -d . -sourcepath tmp/java/src/main/java/ -subpackages org -hdf project.name "libsignal-protocol-java"
 rm -rf ./tmp/
